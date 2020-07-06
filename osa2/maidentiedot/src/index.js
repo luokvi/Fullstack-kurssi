86b4b22a81bed9bc58600d67c7d7380cf8af66f5
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-06 18:32:00 +0300

    Update to match 2.13

--- osa2/maidentiedot/src/index.js
+++ osa2/maidentiedot/src/index.js
@@ -2,36 +2,26 @@ import React, {useState, useEffect}from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios'
 
-const Countries = ({list}) =>{
+const Countries = ({list, setFunc}) =>{
+  
   if (list.length === 1){
     return(
-      list.map(country =>
-      <div key={country.name}>
-        <h1>{country.name}</h1>
-        <p>
-          Capital: {country.capital}<br />
-          Population: {country.population}
-          </p>
-        <h2>languages</h2>
-        <ul>
-          {country.languages.map(lan =>
-            <li key={lan.name}>
-              {lan.name}
-              </li>
-          )
-          }
-        </ul>
-        <img src={country.flag} alt="" width="200" />
-      </div>
-      )
+    list.map(c =>
+        <SingleCountry key={c.name} country={c} />
+        )
     )
   }
+
   if (list.length <= 10){
     return(
       list.map(country =>
-        <p key={country.name}>
+        <div key={country.name}>
+        <p>
           {country.name}
+          <button onClick={() => setFunc(country.name)}>show</button>
         </p>
+        
+        </div>
       )
     )
   }
@@ -42,6 +32,29 @@ const Countries = ({list}) =>{
     </p>
   )
 }
+
+const SingleCountry = ({country}) =>{
+  return(
+    <div key={country.name}>
+      <h1>{country.name}</h1>
+      <p>
+        Capital: {country.capital}<br />
+        Population: {country.population}
+        </p>
+      <h2>languages</h2>
+      <ul>
+        {country.languages.map(lan =>
+          <li key={lan.name}>
+            {lan.name}
+            </li>
+        )
+        }
+      </ul>
+      <img src={country.flag} alt="" width="200" />
+    </div>
+  )
+}
+
 
 const App = () =>{
   const [ newFilter, setFilter] = useState('')
@@ -62,7 +75,7 @@ const App = () =>{
   return(
       <div>
       find countries: <input value={newFilter} onChange={handleFilterInput} />
-      <Countries list={countriesToShow} />
+      <Countries list={countriesToShow} setFunc={setFilter} />
       </div>
   )
 }
