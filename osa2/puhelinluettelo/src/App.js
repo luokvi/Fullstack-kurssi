844ff10167c49895f2390db22a107e526978b20a
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-06 14:49:08 +0300

    Update to match 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,17 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './namesnumbers'
 import NameForm from './nameForm'
 import FilterForm from './filterForm'
 
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-
+  const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [newFilter, setNewFilter ] = useState('')
@@ -53,6 +48,13 @@ const App = () => {
       
   }
 
+  useEffect(() =>{
+    axios.get('http://localhost:3001/persons').then(response =>{
+      setPersons(response.data)
+    })
+  }, [])
+
+  
   return (
     <div>
       <h1>Phonebook</h1>
