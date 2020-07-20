6803ffb937c41b9f4b710a436d4c8ddb7448e89d
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-20 18:09:04 +0300

    Update to match 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -18,7 +18,30 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favouriteBlog = (blogs) => {
+    if (blogs.length === 0){
+        return null
+    }
+    let favourite = {
+        title: blogs[0].title,
+        author: blogs[0].author,
+        likes: blogs[0].likes
+    }
+    blogs.forEach(b => {
+        if (b.likes > favourite.likes){
+            favourite = {
+                title: b.title,
+                author: b.author,
+                likes: b.likes
+            }
+        }
+    })
+
+    return favourite
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favouriteBlog
 }
