63616c68a2ac7595d210906987482a21d3581cc2
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-20 17:28:34 +0300

    Update to match 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,6 +3,22 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length === 0){
+        return 0
+    }
+    if (blogs.length === 1){
+        return blogs[0].likes
+    }
+
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
     dummy,
+    totalLikes,
 }
