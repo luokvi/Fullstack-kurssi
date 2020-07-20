55526877dde1ef868eb9be9bf637839a8433f298
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-20 14:36:15 +0300

    Add app

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,17 +1,7 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
-const blogRouter = require('./controllers/blogs')
-
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
 
 
 const PORT = config.PORT
