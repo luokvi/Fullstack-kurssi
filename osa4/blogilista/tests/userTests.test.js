a68b6a80713b9090b53e857c3797591c50796127
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-23 01:44:48 +0300

    Update to match 4.16

--- osa4/blogilista/tests/userTests.test.js
+++ osa4/blogilista/tests/userTests.test.js
@@ -26,10 +26,7 @@ describe('when there is initially one user at db', () => {
       password: '123',
     }
 
-    await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(200)
+    await api.post('/api/users').send(newUser).expect(200)
       .expect('Content-Type', /application\/json/)
 
     const usersAtEnd = await helper.usersInDb()
@@ -39,7 +36,39 @@ describe('when there is initially one user at db', () => {
     expect(usernames).toContain(newUser.username)
   })
 
-  afterAll(() => {
-    mongoose.connection.close()
+  test('creating a new user with the same username ends in an error', async () => {
+      const newUser = {
+          username: 'root',
+          passwordHash: '123'
+      }
+
+      await api.post('/api/users').send(newUser).expect(400)
   })
+  
+})
+
+describe('not able to create new users with', () => {
+    test('username thats less than 3 characters', async () => {
+        const newUser = {
+            username: 'r',
+            name: 'Testi Kayttaja',
+            passwordHash: '123'
+        }
+
+        await api.post('/api/users').send(newUser).expect(400)
+    })
+
+    test('password that less than 3 characters', async () => {
+        const newUser = {
+            username: 'testi',
+            name: 'Testi Kayttaja',
+            passwordHash: '1'
+        }
+
+        await api.post('/api/users').send(newUser).expect(400)
+    })
+})
+
+afterAll(() => {
+    mongoose.connection.close()
 })
