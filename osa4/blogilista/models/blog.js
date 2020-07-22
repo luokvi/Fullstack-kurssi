b519b30bab662be8bb3d0feffabf80cb9bfc53e3
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-22 16:56:08 +0300

    Update to match 4.9

--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -2,11 +2,18 @@ const mongoose = require('mongoose')
 const config = require('../utils/config')
 
 const blogSchema = mongoose.Schema({
-    id: String,
     title: {type: String, required: true},
     author: String,
     url: {type: String, required: true},
     likes: Number
+  })
+
+  blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
   })
   
   const Blog = mongoose.model('Blog', blogSchema)
