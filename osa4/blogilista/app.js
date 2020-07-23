028f04dac1818f1280bba286d3248dd6cf4aef8f
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-23 18:23:56 +0300

    Update to match 4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,13 +4,16 @@ const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandling)
 
