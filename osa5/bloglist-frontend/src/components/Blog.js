579f74d44c5feef252d7ba4ed2220c1f096d1794
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-28 16:37:17 +0300

    Update to match 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visibleFull, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    setVisible(!visibleFull)
+  }
+
+  if (visibleFull){
+    return(
+      <div className="blog">
+        <p>
+          <b>{blog.title}</b>
+          <button onClick={toggleVisible}>hide</button>
+        </p>
+        <p>{blog.author}</p>
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes}<button>like</button>
+          </p>
+        <p>{blog.user.name}</p>
+      </div>
+    )
+  }
+
+  return (
+  <div className="blog">
     <p>
       <b>{blog.title}</b> by {blog.author}
+      <button onClick={toggleVisible}>view</button>
     </p>
   </div>
-)
+  )
+  }
 
 export default Blog
