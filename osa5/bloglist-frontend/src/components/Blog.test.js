5285f6b98bbf360ff515e8c378e277cb12f3e8fb
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-29 13:56:24 +0300

    Update to match 5.15

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -3,8 +3,9 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-describe('renders Blog with', () => {
+describe('Blog', () => {
   let component
+  const mockLikeHandler = jest.fn()
 
   beforeEach(() => {
     const blog = {
@@ -14,20 +15,30 @@ describe('renders Blog with', () => {
       likes: 33,
       user: { username: 'Lisääjä', name: 'Matti' }
     }
-    component = render( <Blog blog={blog} />)
+    component = render( <Blog blog={blog} likeFunction={mockLikeHandler}/>)
   })
 
-  test('only blog name and author when not opened', () => {
+  test('renders only blog name and author when not opened', () => {
 
     expect(component.container).toHaveTextContent('Blogi testauksesta by Testi Testaaja')
     expect(component.container).not.toHaveTextContent('tested.com 33')
   })
 
-  test('all fields of blog when opened', () => {
+  test('renders all fields of blog when opened', () => {
     const button = component.getByText('view')
     fireEvent.click(button)
 
     expect(component.container).toHaveTextContent(
       'Blogi testauksestahideTesti Testaajatested.com33likeMatti')
+  })
+
+  test('Pressing blogs "like" button is registered', () => {
+    const openBtn = component.getByText('view')
+    fireEvent.click(openBtn)
+    const likeBtn = component.getByText('like')
+    fireEvent.click(likeBtn)
+    fireEvent.click(likeBtn)
+
+    expect(mockLikeHandler.mock.calls).toHaveLength(2)
   })
 })
