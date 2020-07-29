088598cf1051fdff7c9ae170b98755aa435bbcf1
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-29 13:43:52 +0300

    Update to match 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('renders Blog with', () => {
+  let component
 
-  test('only blog name and author when not opened', () => {
+  beforeEach(() => {
     const blog = {
       title: 'Blogi testauksesta',
       author: 'Testi Testaaja',
       url: 'tested.com',
-      likes: 33
+      likes: 33,
+      user: { username: 'Lisääjä', name: 'Matti' }
     }
+    component = render( <Blog blog={blog} />)
+  })
 
-    const component = render( <Blog blog={blog} />)
+  test('only blog name and author when not opened', () => {
 
     expect(component.container).toHaveTextContent('Blogi testauksesta by Testi Testaaja')
     expect(component.container).not.toHaveTextContent('tested.com 33')
+  })
+
+  test('all fields of blog when opened', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent(
+      'Blogi testauksestahideTesti Testaajatested.com33likeMatti')
   })
 })
