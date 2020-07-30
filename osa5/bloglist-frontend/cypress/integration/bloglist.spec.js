7478fc2847c3d84093c71f5df1a61f7f6b99f6c5
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-07-30 15:16:10 +0300

    Update to match 5.21

--- osa5/bloglist-frontend/cypress/integration/bloglist.spec.js
+++ osa5/bloglist-frontend/cypress/integration/bloglist.spec.js
@@ -1,3 +1,4 @@
+const { func } = require("prop-types")
 
 describe('Blog app', function() {
   beforeEach(function() {
@@ -8,6 +9,12 @@ describe('Blog app', function() {
       password: 'psswrd'
     }
     cy.request('POST', 'http://localhost:3003/api/users/', user)
+    const secondUser = {
+      name: 'Other User',
+      username: 'other',
+      password: 'otherpass'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', secondUser)
     cy.visit('http://localhost:3000')
   })
 
@@ -49,21 +56,51 @@ describe('Blog app', function() {
       cy.get('.blog')
         .should('contain', 'uusi blogi by kirjoittaja')
     })
-    describe('and a blog exists', function() {
+    describe('and when a couple blogs exist', function() {
       beforeEach(function() {
         cy.createBlog({
           title: 'a good blog',
           author: 'writer',
-          url: 'duckduckgo.com'
+          url: 'duckduckgo.com',
+          likes: 0
+        })
+        cy.createBlog({
+          title: 'a very liked blog',
+          author: 'celebrity',
+          url: 'wordpress.com',
+          likes: 23
+        })
+        cy.createBlog({
+          title: 'a second blog',
+          author: 'auteur',
+          url: 'google.com',
+          likes: 2
         })
       })
 
-      it('it can be liked', function() {
+      it('one can be liked', function() {
         cy.contains('a good blog').parent().find('button').click()
         cy.contains('0').contains('like').click()
         cy.get('.notif')
           .should('contain', 'liked a good blog, thanks')
         cy.contains('1')
+      })
+
+      it('one can be deleted', function() {
+        cy.contains('a good blog').parent().find('button').click()
+        cy.contains('remove').click()
+        cy.on('window:confirm', (str) => {
+          expect(str).to.eq('Remove blog a good blog by writer?')
+        })
+        cy.get('.notif').should('contain', 'removed a good blog')
+        cy.get('.blog').should('not.contain', 'a good blog')
+      })
+
+      it.only('a blog from another user cannot be deleted', function() {
+        cy.contains('logout').click()
+        cy.login({ username: 'other', password: 'otherpass' })
+        cy.contains('a good blog').parent().find('button').click()
+        cy.get('.blog').should('not.contain', 'remove')
       })
     })
   })
