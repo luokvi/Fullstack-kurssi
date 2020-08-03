a67ba9d419a40895cdcb908665272e5550ab432f
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-08-03 14:11:45 +0300

    Update to match 6.1

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,57 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+
+    const state = initialState
+
+    deepFreeze(state)
+
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('all fields turned to zeroes', () => {
+    const state = {
+      good: 2,
+      ok: 3,
+      bad: 5
+    }
+
+    const action = {
+      type: 'ZERO'
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
