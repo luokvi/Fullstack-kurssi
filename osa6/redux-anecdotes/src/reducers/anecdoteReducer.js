05ab6db179f945c34bc2f540c15c22a17f5e0748
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-08-04 15:45:41 +0300

    Update to match 6.13

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { act } from "react-dom/test-utils"
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -19,9 +10,8 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
@@ -36,8 +26,10 @@ const reducer = (state = initialState, action) => {
         ...anecToVote,
         votes: anecToVote.votes + 1
       }
-
       return state.map(a => a.id !== id ? a : voted)
+
+    case 'INIT':
+      return action.data
 
     default:
       return state
@@ -61,6 +53,13 @@ export const voteForAnecdote = (id) => {
     data: {
       id: id
     }
+  }
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT',
+    data: anecdotes
   }
 }
 
