afa8b053ce767806b737895a70796202b6b544e8
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-08-19 19:55:55 +0300

    Update to match 8.6

--- osa8/library/index.js
+++ osa8/library/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -105,6 +106,15 @@ const typeDefs = gql`
     allBooks (author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -142,6 +152,20 @@ const resolvers = {
     bookCount: (root) => {
       const booksByAuthor = books.filter((b) => b.author === root.name)
       return booksByAuthor.length
+    }
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      if(!authors.find(a => a.name === args.author)){
+        const author = {name: args.author, id: uuid()}
+        authors = authors.concat(author)
+      }
+
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+
+      return book
     }
   }
 }
