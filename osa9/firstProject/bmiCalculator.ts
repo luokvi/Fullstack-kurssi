6df87c3dbf3bf73c08dab84a88d4118ca1750f35
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-08-24 20:36:03 +0300

    Add semicolons

--- osa9/firstProject/bmiCalculator.ts
+++ osa9/firstProject/bmiCalculator.ts
@@ -1,33 +1,33 @@
 const calculateBmi = (height: number, weight: number): string => {
-    const heightInMeters = height / 100
-    const bmi = weight / (heightInMeters * heightInMeters)
+    const heightInMeters = height / 100;
+    const bmi = weight / (heightInMeters * heightInMeters);
 
     if (bmi <= 15){
-        return 'Very severely underweight'
+        return 'Very severely underweight';
     }
     if(bmi < 16){
-        return 'Severly underweight'
+        return 'Severly underweight';
     }
     if (bmi < 18.5){
-        return 'Underweight'
+        return 'Underweight';
     }
     if (bmi < 25){
-        return 'Normal (healthy weight)'
+        return 'Normal (healthy weight)';
     }
     if (bmi < 30){
-        return 'Overweight'
+        return 'Overweight';
     }
     if (bmi < 35){
-        return 'Obese Class I (moderately obese)'
+        return 'Obese Class I (moderately obese)';
     }
     if (bmi < 40){
-        return 'Obese Class II (Severely obese)'
+        return 'Obese Class II (Severely obese)';
     }
     if (bmi > 40){
-        return 'Obese Class III (Very severely obese)'
+        return 'Obese Class III (Very severely obese)';
     }
     
-    return 'BMI is bullshit'
+    return 'Something went wrong';
 }
 
-console.log(calculateBmi(180, 74))
+console.log(calculateBmi(180, 74));
