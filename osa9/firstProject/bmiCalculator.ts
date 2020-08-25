744077ba6b9dd1a88d0b5f25b6b63e2132a7fa19
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-08-25 11:20:33 +0300

    Update to match 9.3

--- osa9/firstProject/bmiCalculator.ts
+++ osa9/firstProject/bmiCalculator.ts
@@ -1,6 +1,6 @@
-const calculateBmi = (height: number, weight: number): string => {
-    const heightInMeters = height / 100;
-    const bmi = weight / (heightInMeters * heightInMeters);
+const calculateBmi = (values: bmiValues): string => {
+    const heightInMeters = values.height / 100;
+    const bmi = values.weight / (heightInMeters * heightInMeters);
 
     if (bmi <= 15){
         return 'Very severely underweight';
@@ -30,4 +30,29 @@ const calculateBmi = (height: number, weight: number): string => {
     return 'Something went wrong';
 }
 
-console.log(calculateBmi(180, 74));
+interface bmiValues {
+    height: number;
+    weight: number;
+}
+
+const validateBmiArguments = (args: Array<String>): bmiValues => {
+    if(args.length < 4) throw new Error('Not enough arguments!');
+  
+    const height = Number(args[2]);
+    const weight = Number(args[3]);
+    if (isNaN(height) || isNaN(weight)) throw new Error('Provided values are not numbers!');
+    else {
+      return{
+        height,
+        weight
+      }
+    }
+    
+  }
+  
+  try {
+    const values = validateBmiArguments(process.argv);
+    console.log(calculateBmi(values));
+  } catch (e) {
+      console.log('Error:', e.message);
+  }
