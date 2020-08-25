57a0b1c66c847f0f24c10c18ac06b8bf0dc67396
Viivi Luokkala <viivi.luokkala@edu.turkuamk.fi>
2020-08-25 11:11:23 +0300

    Update to match 9.3

--- osa9/firstProject/exerciseCalculator.ts
+++ osa9/firstProject/exerciseCalculator.ts
@@ -1,3 +1,7 @@
+interface exerciseValues {
+    exercises: Array<number>;
+    target: number;
+}
 interface exerciseResult {
   periodLength: number;
   trainingDays: number;
@@ -8,27 +12,27 @@ interface exerciseResult {
   average: number;
 }
 
-const calculateExercises = (exercises: Array<number>, target: number): exerciseResult => {
-  const length = exercises.length;
-  const training = exercises.filter(e => e > 0);
+const calculateExercises = (values: exerciseValues): exerciseResult => {
+  const length = values.exercises.length;
+  const training = values.exercises.filter(e => e > 0);
   const trainigLenght = training.length;
-  const sum = exercises.reduce((a, b) =>  a + b);
+  const sum = values.exercises.reduce((a, b) =>  a + b);
   const avg = sum / length;
-  const successfull = avg >= target;
-  const difference = avg - target;
+  const successfull = avg >= values.target;
+  const difference = avg - values.target;
   let rating = 0;
   let ratingDes = '';
   if (difference <= -1){
       rating = 1;
-      ratingDes = 'You can do better'
+      ratingDes = 'You can do better';
   }
   if (difference > -1 && difference <= 0.5){
       rating = 2;
-      ratingDes = 'Good! Keep at it'
+      ratingDes = 'Good! Keep at it';
   }
   if (difference > 0.5){
       rating = 3;
-      ratingDes = 'Amazing!'
+      ratingDes = 'Amazing!';
   }
 
 
@@ -38,9 +42,34 @@ const calculateExercises = (exercises: Array<number>, target: number): exerciseR
       success: successfull,
       rating: rating,
       ratingDescription: ratingDes,
-      target: target,
+      target: values.target,
       average: avg
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+const validateArguments = (args: Array<String>): exerciseValues => {
+  if(args.length < 4) throw new Error('Not enough arguments!');
+
+  const target = Number(process.argv[2]);
+  if (isNaN(target)) throw new Error('Provided values are not numbers!');
+  let exercises = [];
+  let index = 3;
+  while (index < process.argv.length){
+    const value = (Number(process.argv[index]));
+    if(isNaN(value)) throw new Error('Provided values are not numbers!');
+    exercises.push(value);
+    index += 1;
+  }
+
+  return{
+      exercises,
+      target
+  }
+}
+
+try {
+  const values = validateArguments(process.argv);
+  console.log(calculateExercises(values));
+} catch (e) {
+    console.log('Error:', e.message);
+}
